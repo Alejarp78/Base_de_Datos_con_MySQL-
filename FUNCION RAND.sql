dd668d5cf67a779626a81a8316235447378138f8
Jarp <jhozmeraramirez@gmail.com>
2023-05-15 21:57:20 -0400

    Base de datos empresa venta de jugos, clientes, vendedores y sus facturas

---
+++ FUNCION RAND.sql
@@ -0,0 +1,14 @@
+USE empresa;
+
+-- Generar un numero comprendido entre MIN = 20 Y MAX = 250.
+-- (RAND()*(MAX-MIN+1))+MIN
+
+SELECT RAND()*(250-20+1)+20 AS ALEATORIO;
+
+SELECT FLOOR(RAND()*(250-20+1)+20) AS ALEATORIO;
+
+SET GLOBAL log_bin_trust_function_creators = 1;
+
+SELECT f_aleatorio(1,10) AS RESULTADO;
+
+SELECT f_cliente_aleatorio() AS DNI_CLIENTE;
